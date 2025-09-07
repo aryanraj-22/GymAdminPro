02e3d0c0a97b83f0eb3a0a8c0812c18521206997
Aryan Raj <aryan9639@gmail.com>
2025-09-07 11:50:50 +0530

    auth fix

--- middleware/auth.js
+++ middleware/auth.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 
 const auth = async (req, res, next) =>{
     
-    const token = req.cookies.token;
+    const token = req.cookies.token || (req.headers.authorization && req.headers.authorization.split(' ')[1]);
     if(!token){
         return res.status(401).json({ error: 'No token, authorization denied' });
     }else{
